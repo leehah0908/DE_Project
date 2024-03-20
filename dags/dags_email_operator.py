cb70f6bfde944be2cccab5029b5b78f936d9329a
leehah0908 <leehah0908@naver.com>
2024-03-20 16:47:10 +0900

    new docker

---
+++ dags/dags_email_operator.py
@@ -0,0 +1,18 @@
+from airflow import DAG
+import datetime
+import pendulum
+
+from airflow.operators.email import EmailOperator
+
+with DAG(
+    dag_id = "dags_email_operator",
+    schedule = "0 8 1 * *",
+    start_date = pendulum.datetime(2024, 3, 1, tz = "Asia/Seoul"),
+    catchup = False
+) as dag:
+    send_email_task = EmailOperator(
+        task_id = "send_email_task",
+        to = 'leehah0908@naver.com',
+        subject = 'Airflow 성공 메일',
+        html_content = 'Airflow 작업이 완료됨'
+    )
